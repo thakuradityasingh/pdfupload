9a1194b4d9de8ade49d654368b255a08229d6acd
thakuradityasingh <ast1b2910@gmail.com>
2023-10-05 17:14:28 +0530

    update main

--- amplify/backend/function/lambdabot/src/index.py
+++ amplify/backend/function/lambdabot/src/index.py
@@ -1,15 +1,40 @@
 import json
 
-def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+def lambda_handler(event, context):
+    try:
+        # Get the user's question from the Lambda event
+        user_question = event['body']['question']
+
+        # Process the question and generate an answer (replace with your logic)
+        answer = process_question(user_question)
+
+        # Prepare the response
+        response = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",  # Enable CORS for your frontend
+            },
+            "body": json.dumps({"answer": answer})
+        }
+
+        return response
+    except Exception as e:
+        # Handle any errors and return an appropriate error response
+        error_response = {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",  # Enable CORS for your frontend
+            },
+            "body": json.dumps({"error": str(e)})
+        }
+        return error_response
+
+def process_question(question):
+    # Replace this with your own logic to process the question and generate an answer
+    # For this example, we're returning a hardcoded answer
+    if "PDF" in question:
+        return "Sure, I can help you with PDF editing!"
+    else:
+        return "I'm sorry, I don't understand the question."
