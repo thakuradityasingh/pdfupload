101296f6803da6eab3d36cd77bb5543850f61c70
thakuradityasingh <ast1b2910@gmail.com>
2023-10-08 13:38:01 +0530

    update app

--- amplify/backend/function/lambdabot/src/index.py
+++ amplify/backend/function/lambdabot/src/index.py
@@ -1,8 +1,21 @@
-import json
+import boto3
 
 def lambda_handler(event, context):
-    print("Hello Adi")
-    return {
-        "statusCode": 200,
-        "body": "Hello Adi"
-    }
+    # Get the bucket name from the event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+    
+    # List all objects in the bucket
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+    
+    # Extract and print the object keys
+    if 'Contents' in response:
+        object_keys = [obj['Key'] for obj in response['Contents']]
+        print("Uploaded Files:")
+        for key in object_keys:
+            print(key)
+    else:
+        print("No objects found in the bucket.")
+
---
+++ amplify/backend/function/lambdamain/src/index.py
@@ -0,0 +1,38 @@
+import json
+
+def lambda_handler(event, context):
+    try:
+        # Get the user's question from the Lambda event
+        user_question = event['body']['question']
+
+        # Process the question and generate an answer (replace with your logic)
+        answer = process_question(user_question)
+
+        # Prepare the response
+        response = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps({"answer": answer})
+        }
+
+        return response
+    except Exception as e:
+        # Handle any errors and return an appropriate error response
+        error_response = {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps({"error": str(e)})
+        }
+        return error_response
+
+def process_question(question):
+    # Replace this with your own logic to process the question and generate an answer
+    # For this example, we're returning a hardcoded answer
+    if "PDF" in question:
+        return "Sure, I can help you with PDF editing!"
+    else:
+        return "I'm sorry, I don't understand the question."
